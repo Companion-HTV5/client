07a9e6a24cececa7f4d29e367ab491e8528bf8e2
chrisphil335 <61081461+chrisphil335@users.noreply.github.com>
2021-10-25 13:50:51 -0400

    added auth0 login button. It does not currently root to assessment. There is no logout button

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider domain="dev-1phr29mi.us.auth0.com" clientId="oNLolsNpomyzWKwoYb1wyXxZa2I9VkfY" redirectUri={window.location.origin}>
+      <App />
+    </Auth0Provider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
